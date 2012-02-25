f19ba173e6b97dd777a958155c0d3eeb779978c8
Silas Sewell <silas@sewell.org>
2012-02-24 23:53:50 -0500

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from distutils.core import setup
 
-long_description = """
-brpm is a simple tool that makes building RPMs locally a little easier.
-"""
-
 setup(
     name='brpm',
-    version='0.1.0',
+    version='0.2.0',
     description='A tool for building RPM packages',
-    long_description=long_description.strip(),
+    long_description='brpm is a simple tool for building RPMs.',
     author='Silas Sewell',
-    author_email='silas@sewell.ch',
+    author_email='silas@sewell.org',
     license='MIT License',
     url='https://github.com/silas/brpm',
     packages=['brpm'],
